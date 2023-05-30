346e304f5a7578dca88023dce1da223601d7fa7b
Al Slater <al.slater@essiell.com>
2023-05-30 11:16:49 +0100

    Updated runner script

--- get_octopus.sh
+++ get_octopus.sh
@@ -1,6 +1,6 @@
-cd /home/aslate/src/octograph
+cd ~/src/octograph
 
 . .venv/bin/activate
 
-date >> /home/aslate/logs/octograph.log
-python app/octopus_to_influxdb.py >> /home/aslate/logs/octograph.log 2>&1
+date >> ~/logs/octograph.log
+python app/octopus_to_influxdb.py >> ~/logs/octograph.log 2>&1
